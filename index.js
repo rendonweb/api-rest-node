1a092f824f76d778e125cdba772da3bad14cb3a4
Steven Rendon <steven.rendon@hotmail.com>
2025-08-21 09:38:18 -0500

    Add not found middleware

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from "express";
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Bienvenidos a nuestra API REST!");
+app.use((req, res, next) => {
+    console.log(req.method);
+    next();
 });
+
+app.get('/', (req, res) => {
+    res.json({'message':"Bienvenidos a nuestra API REST!"});
+});
+
+import notFound from "./src/middlewares/not-found.js";
+app.use(notFound);
 
 const PORT = 3000;
 
